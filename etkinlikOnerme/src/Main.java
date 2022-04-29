4fdbf63c0264a514f28d406e07c94858ce5ccff2
atmacasoftware <94282227+atmacasoftware@users.noreply.github.com>
2022-04-29 07:40:55 +0300

    Add files via upload

---
+++ etkinlikOnerme/src/Main.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        double weather;
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Hava Kaç Derece?: ");
+        weather = input.nextDouble();
+
+        if(weather < 5){
+            System.out.println("Havalar soğuk... Sana kayak yapmayı öneriyorum!");
+        } else if (weather >= 5 && weather < 15) {
+            System.out.println("Bu havada yapılabilecek en iyi etkinlik sinemaya gitmek!");
+        } else if (weather >= 15 && weather < 25) {
+            System.out.println("Piknik yapmaya gidebilirsin!");
+        }else if(weather >= 25){
+            System.out.println("Hava çok sıcak! Bence yüzmeye gitmelisin");
+        }
+
+    }
+}
